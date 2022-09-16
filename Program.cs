a78133da92a2b1ed2b77f1c9b36e84d2238d5f4f
Pingvu <alka04006@utb.vaxjo.se>
2022-09-16 14:47:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_2._13
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("lön för Abel?");
+            int Abel = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för Nasteho?");
+            int Nasteho = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("lön för Josef?");
+            int Josef = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Abel" + Abel + " kr" + " Nasteho" + Nasteho + " Josef" + Josef + " kr");
+
+            Console.WriteLine("medelönen för ");
+
+            Console.WriteLine(( Abel + Nasteho + Josef) /3 );
+        }
+    }
+}
